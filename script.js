5f720ab906ed3e5c9259e130a73619a3d92a6b0e
Azecko <sami.panseri@epfl.ch>
2023-08-10 14:16:35 +0200

    [feature] Multiple accreditations & phone numbers
    If someone has multiple accreditations or phone numbers, a select will pop and let choose the user which phone number and/or which accreditation he wants to use.
    
    Also made some styling, changed the placeholder of the search bar, and added a little "tutorial" under it.

--- script.js
+++ script.js
@@ -1,20 +1,91 @@
+function changeHTML(user, accreditation, phoneValue) {
+    $('.firstname-name').html(`${user.firstname} ${user.name}`)
+    $('.fonction').html(user.accreds[accreditation].position)
+    $('.epfl-unit').html(user.accreds[accreditation].acronym)
+    $('.office-place').html(user.accreds[accreditation].officeList[0] || 'Not defined')
+    $('.phone').html(user.accreds[accreditation].phoneList[phoneValue])
+    $('.phone').attr('href', `tel:${user.accreds[accreditation].phoneList[phoneValue]}`)
+    $('.email').html(`${user.profile}@epfl.ch`)
+    $('.email').attr('href', `mailto:${user.profile}@epfl.ch`)
+    $('.people').html(`people/${user.profile}`)
+    $('.people').attr('href', `https://people.epfl.ch/${user.sciper}`)
+}
+
+function generateAndChangeHTML(user, accreditation) {
+    changeHTML(user, accreditation, 0)
+
+    let phone = 0;
+    if(user.accreds[accreditation].phoneList.length > 1) {
+        $('.accred-selection-phone').removeClass('d-none');
+        $('#select-accred-phone').empty();
+
+        const accredPhoneSelect = document.getElementById('select-accred-phone')
+        for (let i = 0; i < user.accreds[accreditation].phoneList.length; i++) {
+            let phoneNumber = user.accreds[accreditation].phoneList[i];
+            const option = document.createElement('option');
+            const optionText = document.createTextNode(phoneNumber)
+
+            option.appendChild(optionText)
+            option.setAttribute('value', i)
+
+            accredPhoneSelect.appendChild(option)
+        }
+
+        $("#select-accred-phone").change(function(){
+            var phoneValue = $(this).find("option:selected").attr("value");
+
+            changeHTML(user, accreditation, phoneValue)
+            phone = phoneValue;
+        });
+    } else {
+        $('.accred-selection-phone').addClass('d-none')
+        phone = 0;
+    }
+
+    changeHTML(user, accreditation, phone)
+}
+
+
 function getPeopleBySciper(value) {
     $.get(`https://search-api.epfl.ch/api/ldap?q=${value}`, function( data ) {
         if(!data.length || data.length >= 2) {
             $('.alert-danger').html('Sciper is invalid')
             $('.alert-danger').removeClass('d-none')
         } else {
-            $('.firstname-name').html(`${data[0].firstname} ${data[0].name}`)
-            $('.fonction').html(data[0].accreds[0].position)
-            $('.epfl-unit').html(data[0].accreds[0].acronym)
-            $('.office-place').html(data[0].accreds[0].officeList[0] || `Not defined`)
-            $('.phone').html(data[0].accreds[0].phoneList[0])
-            $('.phone').attr('href', `tel:${data[0].accreds[0].phoneList[0]}`)
-            $('.email').html(`${data[0].profile}@epfl.ch`)
-            $('.email').attr('href', `mailto:${data[0].profile}@epfl.ch`)
-            $('.people').html(`people/${data[0].profile}`)
-            $('.people').attr('href', `https://people.epfl.ch/${data[0].sciper}`)
-            $('.alert-danger').addClass('d-none')
+            if(data[0].accreds.length > 1) {
+                $('.accred-selection').removeClass('d-none')
+                $('.accred-hr').removeClass('d-none')
+
+                $('#select-accred').empty()
+
+                generateAndChangeHTML(data[0], 0)
+
+                const accredSelect = document.getElementById('select-accred')
+                for (let i = 0; i < data[0].accreds.length; i++) {
+                    let accred = data[0].accreds[i]
+                    const option = document.createElement('option');
+                    const optionText = document.createTextNode(accred.name)
+
+                    option.appendChild(optionText)
+                    option.setAttribute('value', i)
+
+                    accredSelect.appendChild(option)
+                }
+
+                $("#select-accred").change(function(){
+                    var optionValue = $(this).find("option:selected").attr("value");
+
+                    generateAndChangeHTML(data[0], optionValue)
+                });
+
+                $('.alert-danger').addClass('d-none')
+                
+            } else {
+                $('.accred-selection').addClass('d-none')
+                $('.accred-hr').addClass('d-none')
+                generateAndChangeHTML(data[0], 0)
+                $('.alert-danger').addClass('d-none')
+            }
         }
     });
 }
